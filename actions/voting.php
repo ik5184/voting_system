66c16f331a3d461f13c930e7cebcd40b0c56031d
Iklash khan <111762117+ik5184@users.noreply.github.com>
2023-08-13 17:13:05 +0530

    voting.php

--- actions/voting.php
+++ actions/voting.php
@@ -5,31 +5,43 @@
   $totalvotes=$votes+1;
   $gid=$_POST['groupid'];
   $uid=$_SESSION['id'];
-$sql= "update `userdata` set votes='$totalvotes' where id='$gid';";
-  $updatevote=mysqli_query($con,$sql);
+//   $m=$data['status'];           
+  
 
-  $sql1= "update `userdata` set status=1 where id='$uid';";
- $updatestatus=mysqli_query($con,$sql1);
+ if($_SESSION['status']!=1){
+   $updatevote=mysqli_query($con,"update `userdata` set votes='$totalvotes' where id='$gid'");
 
- if($updatestatus and $updatevote){
-    $sql="SELECT username,photo,votes,id from `userdata` WHERE standard='group';";
-    $resultgroup=mysqli_query($con,$sql);               
-    $group=mysqli_fetch_all($resultgroup,MYSQLI_ASSOC);  
-    $_SESSION['group']=$group;  
-    $_SESSION['status']=1;
-    echo '<script>
-    alert("voting successfully");
-    window.location="../partial/dashboard.php"
-    
-    </script>';
-
+  
+   $updatestatus=mysqli_query($con,"update `userdata` set status=1 where id='$uid'");
+  
+   if($updatevote and $updatestatus){
+      $sql="SELECT username,photo,votes,id from `userdata` WHERE standard='group';";
+      $resultgroup=mysqli_query($con,$sql);               
+      $group=mysqli_fetch_all($resultgroup,MYSQLI_ASSOC);  
+      $_SESSION['group']=$group;  
+      $_SESSION['status']=1;
+      echo '<script>
+      alert("voting successfully");
+      window.location="../partial/dashboard.php"
+      
+      </script>';
+  
+   }
+   else{
+      echo '<script>
+      alert("technical error");
+      window.location="../partial/dashboard.php"
+      
+      </script>';
+   }
+  
  }
  else{
-    echo '<script>
-    alert("technical error");
-    window.location="../partial/dashboard.php"
-    
-    </script>';
- }
 
+   echo '<script>alert("you already voted")
+   
+   window.location="../";
+   </script>';
+ }
+  
 ?>
